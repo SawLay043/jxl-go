461990407e43db3c88e9d10c28531d5e496ee5cf
SawLay043 <laysaw043@gmail.com>
2026-03-20 21:30:00 +0630

    test(frame): increase coverage for LFGlobal and ModularStream transforms
    Added tests for all RCT types, Palette, and Squeeze transforms in ModularStream. Increased frame package coverage to 81.8%. Fixed mock data in LFGlobal tests.

--- frame/ModularStream_extra_test.go
+++ frame/ModularStream_extra_test.go
@@ -3,146 +3,192 @@ package frame
 import (
 	"testing"
 
+	"github.com/kpfaulkner/jxl-go/bundle"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestInverseHorizontalSqueeze_ExtraWidth(t *testing.T) {
-	// orig: 1x2, res: 1x1 -> channel: 1x3
-	orig := NewModularChannelWithAllParams(1, 2, 0, 0, false)
-	orig.allocate()
-	orig.buffer[0][0] = 10
-	orig.buffer[0][1] = 50
+func TestApplyTransforms_RCT_AllTypes(t *testing.T) {
+	for rctType := 0; rctType <= 6; rctType++ {
+		t.Run(string(rune('0'+rctType)), func(t *testing.T) {
+			ms := &ModularStream{
+				transforms: []TransformInfo{
+					{
+						tr:      RCT,
+						beginC:  0,
+						rctType: rctType,
+					},
+				},
+				channels: []*ModularChannel{
+					NewModularChannelWithAllParams(1, 1, 0, 0, false),
+					NewModularChannelWithAllParams(1, 1, 0, 0, false),
+					NewModularChannelWithAllParams(1, 1, 0, 0, false),
+				},
+			}
+			for i := 0; i < 3; i++ {
+				ms.channels[i].allocate()
+				ms.channels[i].buffer[0][0] = int32(i + 10) // 10, 11, 12
+			}
 
-	res := NewModularChannelWithAllParams(1, 1, 0, 0, false)
-	res.allocate()
-	res.buffer[0][0] = 2
-
-	outputInfo := NewModularChannelWithAllParams(1, 3, 0, 0, false)
-	output, err := inverseHorizontalSqueeze(outputInfo, orig, res)
-	require.NoError(t, err)
-	require.NotNil(t, output)
-	assert.Equal(t, uint32(3), output.size.Width)
-	
-	// With the fix: left = avg = 10. nextAvg = 50.
-	// a=10, b=10, c=50.
-	// a <= b <= c -> true.
-	// x = (4*10 - 3*50 - 10 - 6) / 12 = -126 / 12 = -10.
-	// d = 2*(10-10)=0. e=2*(10-50)=-80.
-	// x+(x&1) = -10 + 0 = -10. -10 < 0 -> true. x = d-1 = -1.
-	// x-(x&1) = -10 - 0 = -10. -10 < -80 -> false.
-	// returns -1.
-	// diff = 2 + (-1) = 1.
-	// first = 10 + 1/2 = 10.
-	// buffer[0][0] = 10, buffer[0][1] = 10 - 1 = 9
-	assert.Equal(t, int32(10), output.buffer[0][0])
-	assert.Equal(t, int32(9), output.buffer[0][1])
-	assert.Equal(t, int32(50), output.buffer[0][2])
-}
-
-func TestTendancy(t *testing.T) {
-	testCases := []struct {
-		left, avg, nextAvg int32
-		expected           int32
-	}{
-		{15, 10, 5, 3},
-		{5, 10, 15, -3},
-		{10, 5, 10, 0},
-	}
-	for _, tc := range testCases {
-		assert.Equal(t, tc.expected, tendancy(tc.left, tc.avg, tc.nextAvg), "tendancy(%d, %d, %d)", tc.left, tc.avg, tc.nextAvg)
+			err := ms.applyTransforms()
+			assert.Nil(t, err)
+			assert.NotNil(t, ms.channels[0].buffer[0][0])
+		})
 	}
 }
 
-func TestInverseHorizontalSqueeze_Corrupted(t *testing.T) {
-	orig := NewModularChannelWithAllParams(2, 1, 0, 0, false)
-	res := NewModularChannelWithAllParams(2, 1, 0, 0, false)
-	// Output width should be 2, but we pass 3
-	outputInfo := NewModularChannelWithAllParams(2, 3, 0, 0, false)
-	_, err := inverseHorizontalSqueeze(outputInfo, orig, res)
-	assert.Error(t, err)
-}
 
-func TestInverseVerticalSqueeze_Corrupted(t *testing.T) {
-	orig := NewModularChannelWithAllParams(1, 2, 0, 0, false)
-	res := NewModularChannelWithAllParams(1, 2, 0, 0, false)
-	// Output height should be 2, but we pass 3
-	outputInfo := NewModularChannelWithAllParams(3, 2, 0, 0, false)
-	_, err := inverseVerticalSqueeze(outputInfo, orig, res)
-	assert.Error(t, err)
-}
-
-func TestInverseHorizontalSqueeze(t *testing.T) {
-	// orig: 2x1, res: 2x1 -> channel: 2x2
-	orig := NewModularChannelWithAllParams(2, 1, 0, 0, false)
-	orig.allocate()
-	orig.buffer[0][0] = 10
-	orig.buffer[1][0] = 20
-
-	res := NewModularChannelWithAllParams(2, 1, 0, 0, false)
-	res.allocate()
-	res.buffer[0][0] = 2
-	res.buffer[1][0] = 4
-
-	outputInfo := NewModularChannelWithAllParams(2, 2, 0, 0, false)
-	output, err := inverseHorizontalSqueeze(outputInfo, orig, res)
-	require.NoError(t, err)
-	require.NotNil(t, output)
-	assert.Equal(t, uint32(2), output.size.Width)
-	assert.Equal(t, uint32(2), output.size.Height)
+func TestApplyTransforms_Palette(t *testing.T) {
+	// Setup Palette transform
+	// PALETTE transform in applyTransforms expects:
+	// ms.channels[0] to be the palette (meta channel)
+	// ms.channels[1...] to be the indexed channels
 	
-	assert.Equal(t, int32(11), output.buffer[0][0])
-	assert.Equal(t, int32(9), output.buffer[0][1])
+	palette := NewModularChannelWithAllParams(3, 2, -1, -1, false) // 3 components, 2 colors
+	palette.allocate()
+	palette.buffer[0][0] = 10 // Component 0, Color 0
+	palette.buffer[0][1] = 20 // Component 0, Color 1
+	palette.buffer[1][0] = 30 // Component 1, Color 0
+	palette.buffer[1][1] = 40 // Component 1, Color 1
+	palette.buffer[2][0] = 50 // Component 2, Color 0
+	palette.buffer[2][1] = 60 // Component 2, Color 1
+
+	indexed := NewModularChannelWithAllParams(1, 1, 0, 0, false)
+	indexed.allocate()
+	indexed.buffer[0][0] = 1 // Color 1
+
+	ms := &ModularStream{
+		frame: NewFakeFramer(MODULAR),
+		transforms: []TransformInfo{
+			{
+				tr:        PALETTE,
+				beginC:    0,
+				numC:      3,
+				nbColours: 2,
+				nbDeltas:  0,
+			},
+		},
+		channels: []*ModularChannel{palette, indexed},
+	}
+	// Inject bitDepth
+	ms.frame.(*FakeFramer).imageHeader.BitDepth = &bundle.BitDepthHeader{BitsPerSample: 8}
+
+	err := ms.applyTransforms()
+	assert.Nil(t, err)
+	
+	// Palette transform replaces indexed channel with numC channels
+	// and removes the palette channel from the beginning.
+	assert.Equal(t, 3, len(ms.channels))
+	assert.Equal(t, int32(20), ms.channels[0].buffer[0][0])
+	assert.Equal(t, int32(40), ms.channels[1].buffer[0][0])
+	assert.Equal(t, int32(60), ms.channels[2].buffer[0][0])
 }
 
-func TestInverseVerticalSqueeze(t *testing.T) {
-	// orig: 1x2, res: 1x2 -> channel: 2x2
-	orig := NewModularChannelWithAllParams(1, 2, 0, 0, false)
+func TestApplyTransforms_SqueezeHorizontal(t *testing.T) {
+	// Setup Squeeze Horizontal
+	orig := NewModularChannelWithAllParams(1, 1, 0, 1, false)
 	orig.allocate()
 	orig.buffer[0][0] = 10
-	orig.buffer[0][1] = 20
+	
+	residu := NewModularChannelWithAllParams(1, 1, 0, 1, false)
+	residu.allocate()
+	residu.buffer[0][0] = 2
 
-	res := NewModularChannelWithAllParams(1, 2, 0, 0, false)
-	res.allocate()
-	res.buffer[0][0] = 2
-	res.buffer[0][1] = 4
-
-	outputInfo := NewModularChannelWithAllParams(2, 2, 0, 0, false)
-	output, err := inverseVerticalSqueeze(outputInfo, orig, res)
-	require.NoError(t, err)
-	require.NotNil(t, output)
-	assert.Equal(t, uint32(2), output.size.Width)
-	assert.Equal(t, uint32(2), output.size.Height)
-
-	assert.Equal(t, int32(11), output.buffer[0][0])
-	assert.Equal(t, int32(9), output.buffer[1][0])
-}
-
-func TestApplyTransforms_Squeeze(t *testing.T) {
 	ms := &ModularStream{
 		transforms: []TransformInfo{
-			{tr: SQUEEZE},
+			{
+				tr: SQUEEZE,
+				sp: []SqueezeParam{{horizontal: true, inPlace: true, beginC: 0, numC: 1}},
+			},
 		},
-		squeezeMap: make(map[int][]SqueezeParam),
-		channels: []*ModularChannel{
-			NewModularChannelWithAllParams(2, 1, 0, 0, false), // orig
-			NewModularChannelWithAllParams(2, 1, 0, 0, false), // res
+		channels: []*ModularChannel{orig, residu},
+		squeezeMap: map[int][]SqueezeParam{
+			0: {{horizontal: true, inPlace: true, beginC: 0, numC: 1}},
 		},
-	}
-	ms.channels[0].allocate()
-	ms.channels[0].buffer[0][0] = 10
-	ms.channels[0].buffer[1][0] = 20
-	ms.channels[1].allocate()
-	ms.channels[1].buffer[0][0] = 2
-	ms.channels[1].buffer[1][0] = 4
-
-	ms.squeezeMap[0] = []SqueezeParam{
-		{horizontal: true, numC: 1, beginC: 0, inPlace: true},
 	}
 
 	err := ms.applyTransforms()
-	require.NoError(t, err)
-	assert.Len(t, ms.channels, 1)
+	assert.Nil(t, err)
+	
+	// Inverse horizontal squeeze: 
+	// diff = residu + tendancy(...) = 2 + 0 = 2
+	// first = avg + diff/2 = 10 + 1 = 11
+	// second = first - diff = 11 - 2 = 9
+	assert.Equal(t, 1, len(ms.channels))
 	assert.Equal(t, uint32(2), ms.channels[0].size.Width)
 	assert.Equal(t, int32(11), ms.channels[0].buffer[0][0])
+	assert.Equal(t, int32(9), ms.channels[0].buffer[0][1])
+}
+
+func TestApplyTransforms_SqueezeVertical(t *testing.T) {
+	// Setup Squeeze Vertical
+	orig := NewModularChannelWithAllParams(1, 1, 1, 0, false)
+	orig.allocate()
+	orig.buffer[0][0] = 20
+	
+	residu := NewModularChannelWithAllParams(1, 1, 1, 0, false)
+	residu.allocate()
+	residu.buffer[0][0] = 4
+
+	ms := &ModularStream{
+		transforms: []TransformInfo{
+			{
+				tr: SQUEEZE,
+				sp: []SqueezeParam{{horizontal: false, inPlace: true, beginC: 0, numC: 1}},
+			},
+		},
+		channels: []*ModularChannel{orig, residu},
+		squeezeMap: map[int][]SqueezeParam{
+			0: {{horizontal: false, inPlace: true, beginC: 0, numC: 1}},
+		},
+	}
+
+	err := ms.applyTransforms()
+	assert.Nil(t, err)
+	
+	// Inverse vertical squeeze: 
+	// diff = residu + tendancy(...) = 4 + 0 = 4
+	// first = avg + diff/2 = 20 + 2 = 22
+	// second = first - diff = 22 - 4 = 18
+	assert.Equal(t, 1, len(ms.channels))
+	assert.Equal(t, uint32(2), ms.channels[0].size.Height)
+	assert.Equal(t, int32(22), ms.channels[0].buffer[0][0])
+	assert.Equal(t, int32(18), ms.channels[0].buffer[1][0])
+}
+
+func TestInverseHorizontalSqueeze_Tendancy(t *testing.T) {
+	orig := NewModularChannelWithAllParams(1, 2, 0, 1, false)
+	orig.allocate()
+	orig.buffer[0][0] = 10
+	orig.buffer[0][1] = 20
+	
+	residu := NewModularChannelWithAllParams(1, 2, 0, 1, false)
+	residu.allocate()
+	residu.buffer[0][0] = 2
+	residu.buffer[0][1] = 4
+
+	outputInfo := NewModularChannelWithAllParams(1, 4, 0, 0, false)
+	
+	out, err := inverseHorizontalSqueeze(outputInfo, orig, residu)
+	assert.Nil(t, err)
+	assert.NotNil(t, out)
+	assert.Equal(t, uint32(4), out.size.Width)
+}
+
+func TestInverseVerticalSqueeze_Tendancy(t *testing.T) {
+	orig := NewModularChannelWithAllParams(2, 1, 1, 0, false)
+	orig.allocate()
+	orig.buffer[0][0] = 10
+	orig.buffer[1][0] = 20
+	
+	residu := NewModularChannelWithAllParams(2, 1, 1, 0, false)
+	residu.allocate()
+	residu.buffer[0][0] = 2
+	residu.buffer[1][0] = 4
+
+	outputInfo := NewModularChannelWithAllParams(4, 1, 0, 0, false)
+	
+	out, err := inverseVerticalSqueeze(outputInfo, orig, residu)
+	assert.Nil(t, err)
+	assert.NotNil(t, out)
+	assert.Equal(t, uint32(4), out.size.Height)
 }
